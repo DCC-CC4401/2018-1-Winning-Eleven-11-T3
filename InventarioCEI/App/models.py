ee19567e29c2ecfc053c6cb6189f8ba6262080ec
javier-op <javiermoralesr95@gmail.com>
2018-05-06 17:39:03 -0300

    top pedidos
    cambio modelo articulos y agrego top pedidos al template articulos.html

--- InventarioCEI/App/models.py
+++ InventarioCEI/App/models.py
@@ -3,8 +3,14 @@ from django.db import models
 
 # Create your models here.
 class Articulo(models.Model):
+    opciones = (
+        ("Disponible", "Disponible"),
+        ("En prestamo", "En prestamo"),
+        ("En reparacion", "En reparacion"),
+        ("Perdido", "Perdido"),
+    )
     nombre = models.CharField(max_length=20)
-    estado = models.CharField(max_length=10)
+    estado = models.CharField(max_length=15, choices=opciones, default="Disponible")
     solicitudes = models.IntegerField(default=0)
     imagen = models.ImageField(upload_to="img", default='img/none.png')
 
