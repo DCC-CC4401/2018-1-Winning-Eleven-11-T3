de22d4e78fca9c1fb3d18c75de765f861dfba19d
Javier Morales <javiermoralesr95@gmail.com>
2018-05-07 16:01:00 -0300

    Agrego barras de busqueda.

--- InventarioCEI/App/views.py
+++ InventarioCEI/App/views.py
@@ -4,8 +4,22 @@ from .models import Articulo
 
 # Create your views here.
 def buscar_articulos(request):
-    populares = Articulo.objects.all().order_by("-solicitudes")[:5]
     context = {
-        'populares': populares,
+        'modo': 'basico',
     }
+    if request.method == 'POST':
+        if request.POST.get('modo', False) == "1":
+            context['modo'] = 'avanzado'
+        else:
+            context['modo'] = 'basico'
+        busqueda = Articulo.objects
+        if request.POST.get('nombre', False):
+            busqueda = busqueda.filter(nombre=request.POST['nombre'])
+            context['busqueda'] = busqueda
+        if request.POST.get('estado', False):
+            if request.POST['estado'] != 'any':
+                busqueda = busqueda.filter(nombre=request.POST['estado'])
+                context['busqueda'] = busqueda
+    populares = Articulo.objects.all().order_by("-solicitudes")[:5]
+    context['populares'] = populares
     return render(request, 'articulos.html', context)
