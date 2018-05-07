979345713fb1b355ddbae6257cd4f078790b9760
Javier Morales <javiermoralesr95@gmail.com>
2018-05-07 20:13:43 -0300

    Separo busqueda avanzada.
    Creo nuevo template para busqueda avanzada, cambio el numero de objetos mostrados por fila para que calze con el grid.

--- InventarioCEI/App/urls.py
+++ InventarioCEI/App/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.buscar_articulos, name='articulos'),
+    path('', views.busqueda_simple, name='simple'),
+    path('avanzada/', views.busqueda_avanzada, name='avanzada')
 ]
--- InventarioCEI/App/views.py
+++ InventarioCEI/App/views.py
@@ -3,23 +3,31 @@ from .models import Articulo
 
 
 # Create your views here.
-def buscar_articulos(request):
-    context = {
-        'modo': 'basico',
-    }
+def busqueda_simple(request):
+    context = {}
     if request.method == 'POST':
-        if request.POST.get('modo', False) == "1":
-            context['modo'] = 'avanzado'
-        else:
-            context['modo'] = 'basico'
-        busqueda = Articulo.objects
+        busqueda = Articulo.objects.all()
         if request.POST.get('nombre', False):
             busqueda = busqueda.filter(nombre=request.POST['nombre'])
-            context['busqueda'] = busqueda
-        if request.POST.get('estado', False):
-            if request.POST['estado'] != 'any':
-                busqueda = busqueda.filter(nombre=request.POST['estado'])
-                context['busqueda'] = busqueda
-    populares = Articulo.objects.all().order_by("-solicitudes")[:5]
+            context['busqueda'] = busqueda[:12]
+    populares = Articulo.objects.all().order_by("-solicitudes")[:6]
     context['populares'] = populares
-    return render(request, 'articulos.html', context)
+    return render(request, 'articulos-bsimple.html', context)
+
+
+def busqueda_avanzada(request):
+    context = {}
+    if request.method == 'POST':
+        busqueda = Articulo.objects.all()
+        if request.POST.get('nombre', False):
+            busqueda = busqueda.filter(nombre=request.POST['nombre'])
+            context['busqueda'] = busqueda[:12]
+        if request.POST.get('estado', False):
+            if request.POST['estado'] == 'any':
+                context['busqueda'] = busqueda[:12]
+            else:
+                busqueda = busqueda.filter(estado=request.POST['estado'])
+                context['busqueda'] = busqueda[:12]
+    populares = Articulo.objects.all().order_by("-solicitudes")[:6]
+    context['populares'] = populares
+    return render(request, 'articulos-bavanzada.html', context)
