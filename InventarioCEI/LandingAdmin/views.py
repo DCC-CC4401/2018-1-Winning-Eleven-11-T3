06e1453ef141f721366a90660fc34a77b79ba583
Vokturz <victornavarrokp47@gmail.com>
2018-05-07 23:17:59 -0300

    el calendario puede ser ahora filtrado por espacios

--- InventarioCEI/LandingAdmin/views.py
+++ InventarioCEI/LandingAdmin/views.py
@@ -4,18 +4,26 @@ import json
 
 def index(request):
     reservas = Reserva.objects.all()
+    espacios = Espacio.objects.all()
     reservas_list = []
+    filtroEspacio = []
+    if request.method == 'POST':
+        for posts in request.POST:
+            if "optcheck" in posts:
+                filtroEspacio.append(int(request.POST[posts]))
 
     for res in reservas:
-        reserva_dic = {
-            "title": str(res.id_espacio)+"-"+str(res.id_usuario),
-            "start": res.fecha_inicio.strftime("%Y-%m-%dT%H:%M:%S"),
-            "end": res.fecha_termino.strftime("%Y-%m-%dT%H:%M:%S"),
-            "color": res.id_espacio.color_calendario
-        }
-        reservas_list.append(reserva_dic)
+        if res.id_espacio_id in filtroEspacio:
+            reserva_dic = {
+                "title": str(res.id_espacio)+"-"+str(res.id_usuario),
+                "start": res.fecha_inicio.strftime("%Y-%m-%dT%H:%M:%S"),
+                "end": res.fecha_termino.strftime("%Y-%m-%dT%H:%M:%S"),
+                "color": res.id_espacio.color_calendario
+            }
+            reservas_list.append(reserva_dic)
 
     return render(request, 'landingAdmin/index.html', {'prestamos': Prestamo.objects.all(),
                                                        'articulos': Articulo.objects.all(),
                                                        'reservas': reservas_list,
-                                                       'espacios': Espacio.objects.all()})
+                                                       'espacios': Espacio.objects.all(),
+                                                        'filtroEspacio': filtroEspacio})
