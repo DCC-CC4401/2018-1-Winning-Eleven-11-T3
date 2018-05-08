671da2a5987b3f642962987e195a75970643e25b
Seba Arriola <sarriola@ing.uchile.cl>
2018-05-08 03:01:13 -0300

    Algo de progreso en la ficha del articulo

--- InventarioCEI/ficha/urls.py
+++ InventarioCEI/ficha/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.buscar_articulos, name='articulos'),
+    path('<int:anId>/', views.viewitem, name='viewitem'),
 ]
--- InventarioCEI/ficha/views.py
+++ InventarioCEI/ficha/views.py
@@ -3,23 +3,8 @@ from .models import Articulo
 
 
 # Create your views here.
-def buscar_articulos(request):
-    context = {
-        'modo': 'basico',
-    }
-    if request.method == 'POST':
-        if request.POST.get('modo', False) == "1":
-            context['modo'] = 'avanzado'
-        else:
-            context['modo'] = 'basico'
-        busqueda = Articulo.objects
-        if request.POST.get('nombre', False):
-            busqueda = busqueda.filter(nombre=request.POST['nombre'])
-            context['busqueda'] = busqueda
-        if request.POST.get('estado', False):
-            if request.POST['estado'] != 'any':
-                busqueda = busqueda.filter(nombre=request.POST['estado'])
-                context['busqueda'] = busqueda
-    populares = Articulo.objects.all().order_by("-solicitudes")[:5]
-    context['populares'] = populares
-    return render(request, 'articulos.html', context)
+
+def viewitem(request, anId):
+    anItem = Articulo.objects.get(id=anId)
+    return render(request, 'viewitem.html', {'anItem': anItem})
+    #do something with this user
