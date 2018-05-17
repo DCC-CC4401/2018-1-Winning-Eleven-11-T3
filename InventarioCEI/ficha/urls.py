ef7ae83c5ee6703fbc4eeffa8d0d325aa63cea5f
Seba Arriola <sarriola@ing.uchile.cl>
2018-05-17 18:50:01 -0400

    Admin puede modificar titulo, estado y descripcion.

--- InventarioCEI/ficha/urls.py
+++ InventarioCEI/ficha/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path('<int:anId>/', views.viewitem, name='viewitem'),
+    path('ajax/guardar_titulo/', views.guardar_titulo, name='guardar_titulo'),
+    path('ajax/guardar_descripcion/', views.guardar_descripcion, name='guardar_descripcion'),
+    path('ajax/cambiar_estado/', views.cambiar_estado, name='cambiar_estado'),
+
 ]
--- InventarioCEI/ficha/views.py
+++ InventarioCEI/ficha/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from ficha.models import Prestables
-
+from django.http import JsonResponse
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -15,3 +16,56 @@ def viewitem(request, anId):
 
 
     return render(request, 'userSystem/home.html')
+
+
+def guardar_titulo(request):
+
+    if request.method == "POST":
+        theId = request.POST.get('id')
+        theTitle = request.POST.get('nombre')
+
+        data = {
+            'modificar_titulo': Prestables.objects.filter(id=theId).update(nombre=theTitle)
+        }
+        if data['modificar_titulo']:
+            data['a_message'] = 'Titulo actualizado correctamente.'
+        else:
+            data['a_message'] = 'Error al actualizar el titulo.'
+
+    return JsonResponse(data)
+
+
+def guardar_descripcion(request):
+
+    if request.method == "POST":
+        theId = request.POST.get('id')
+        theDescription = request.POST.get('descripcion')
+        theDescription = theDescription[:200]
+
+
+        data = {
+            'modificar_descripcion': Prestables.objects.filter(id=theId).update(descripcion=theDescription)
+        }
+        if data['modificar_descripcion']:
+            data['a_message'] = 'Descripcion actualizada correctamente.'
+        else:
+            data['a_message'] = 'Error al actualizar la descripcion.'
+
+    return JsonResponse(data)
+
+
+def cambiar_estado(request):
+
+    if request.method == "POST":
+        theId = request.POST.get('id')
+        theEstado = request.POST.get('estado')
+
+        data = {
+            'modificar_estado': Prestables.objects.filter(id=theId).update(estado=theEstado)
+        }
+        if data['modificar_estado']:
+            data['a_message'] = 'Estado actualizado correctamente.'
+        else:
+            data['a_message'] = 'Error al actualizar el estado.'
+
+    return JsonResponse(data)
