ef0553c11aeec651c457b280157803e8810b0abc
Javier Morales <javiermoralesr95@gmail.com>
2018-06-27 12:08:00 -0400

    ya no aperecen rechazadas en calendario
    Co-Authored-By: Víctor Navarro Aránguiz <vokturz@users.noreply.github.com>

--- InventarioCEI/App/admin.py
+++ InventarioCEI/App/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Prestables
+from .models import Prestables, Prestamos, Solicitudes, Aforo
 
 # Register your models here.
 
 admin.site.register(Prestables)
+admin.site.register(Prestamos)
+admin.site.register(Solicitudes)
+admin.site.register(Aforo)
