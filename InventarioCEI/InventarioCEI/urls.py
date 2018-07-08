861cd414f5421282305d1b6a226aeaaf1f62bb09
javier-op <javiermoralesr95@gmail.com>
2018-07-08 19:28:36 -0400

    casi

--- InventarioCEI/InventarioCEI/urls.py
+++ InventarioCEI/InventarioCEI/urls.py
@@ -19,12 +19,11 @@ from django.conf.urls.static import static
 from userSystem.views import userSystem, usuarios, administrador
 
 urlpatterns = [
-    path('', include('userSystem.urls')),
+    path('', include('App.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', userSystem.SignUpView.as_view(), name='signup'),
     path('accounts/signup/usuarios/', usuarios.UsuariosSignUpView.as_view(), name='usuarios_signup'),
     path('accounts/signup/administrador54r3464643w3frrhey54etfegrtjh46e4t53rf44g53nh5gwfedfgfh4634fefvdhfth45634rwg/',
          administrador.AdministradorSignUpView.as_view(), name='administrador_signup'),
-    path('articulos/', include('App.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
