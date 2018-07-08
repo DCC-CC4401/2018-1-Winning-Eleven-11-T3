f0f05c83bcad61578df1a7431407291111184bea
javier-op <javiermoralesr95@gmail.com>
2018-07-08 19:26:37 -0400

    hice mal el merge :)

--- InventarioCEI/InventarioCEI/urls.py
+++ InventarioCEI/InventarioCEI/urls.py
@@ -1,5 +1,4 @@
 """InventarioCEI URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
@@ -14,25 +13,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-=======
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from userSystem.views import userSystem, usuarios, administrador
 
 urlpatterns = [
-    path('', include('App.urls')),
+    path('', include('userSystem.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', userSystem.SignUpView.as_view(), name='signup'),
     path('accounts/signup/usuarios/', usuarios.UsuariosSignUpView.as_view(), name='usuarios_signup'),
     path('accounts/signup/administrador54r3464643w3frrhey54etfegrtjh46e4t53rf44g53nh5gwfedfgfh4634fefvdhfth45634rwg/',
          administrador.AdministradorSignUpView.as_view(), name='administrador_signup'),
+    path('articulos/', include('App.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> version4
